bce043cdf094cfd67f8c0ee9d7b15795a23fd6bd
Jakub Musialowski <mds@mds.biz.pl>
2025-03-03 22:35:34 +0100

    Prepare app for development

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './common/utils/theme.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 )
