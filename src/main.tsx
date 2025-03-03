4eddf1e6757d76e2fafe2788af9db7cb37fd26e7
Jakub Musialowski <mds@mds.biz.pl>
2025-03-03 22:40:24 +0100

    Added global styled component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './common/utils/theme.ts'
 import App from './App.tsx'
+import StyledGlobal from './styledGlobal.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <StyledGlobal />
       <App />
     </ThemeProvider>
   </StrictMode>
