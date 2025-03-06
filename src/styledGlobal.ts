cb38c852801339d28b97f9e54bd3a947b3f28e7a
Jakub Musialowski <mds@mds.biz.pl>
2025-03-06 21:10:25 +0100

    Add font family to global style

--- src/styledGlobal.ts
+++ src/styledGlobal.ts
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const StyledGlobal = createGlobalStyle`
     html{
         box-sizing: border-box;
+        font-family: "Inter", sans-serif;
     }
 
     *, ::before, ::after {
