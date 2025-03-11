abf38d394d0bf35aff450b0f41be2774c99dfe86
Jakub Musialowski <mds@mds.biz.pl>
2025-03-11 22:00:46 +0100

    Moved theme provider and global styles to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import ListBlock from "./components/ListBlock"
 import { StyledAppWrapper } from "./styledApp"
 import { skillList } from "./common/utils/skillsList"
 import { wantToLernList } from "./common/utils/wantToLernList"
+import { ThemeProvider } from "styled-components"
+import { lightTheme, darkTheme } from "./common/utils/theme"
+import StyledGlobal from "./styledGlobal"
 
 function App() {
 
   return (
-    <StyledAppWrapper>
-      <h1>Personal HomePage</h1>
-      <ListBlock title="My skillset includes 🛠️" content={skillList} />
-      <ListBlock title="What I want to learn next 🚀" content={wantToLernList} />
-    </StyledAppWrapper>
+    <ThemeProvider theme={lightTheme}>
+      <StyledGlobal />
+      <StyledAppWrapper>
+        <h1>Personal HomePage</h1>
+        <ListBlock title="My skillset includes 🛠️" content={skillList} />
+        <ListBlock title="What I want to learn next 🚀" content={wantToLernList} />
+      </StyledAppWrapper>
+    </ThemeProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import { theme } from './common/utils/theme.ts'
 import App from './App.tsx'
-import StyledGlobal from './styledGlobal.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <StyledGlobal />
-      <App />
-    </ThemeProvider>
+    <App />
   </StrictMode>
 )
