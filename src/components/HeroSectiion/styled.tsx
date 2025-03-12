761540982c9538620df4a47a90429ab80040292b
Jakub Musialowski <mds@mds.biz.pl>
2025-03-12 22:08:02 +0100

    Stylng hero elements

--- src/components/HeroSectiion/styled.tsx
+++ src/components/HeroSectiion/styled.tsx
@@ -3,8 +3,13 @@ import styled from "styled-components";
 export const StyledHeroSection = styled.div`
     width: 100%;
 `
+export const StyledHeroContent = styled.div`
+    display: flex;
+    flex-direction: c;
+`
 
 export const StyledHeroIntro = styled.p`
+    margin-block: 0 12px;
     font-size: 12px;
     font-weight: 700;
     line-height: 1.3;
@@ -13,6 +18,7 @@ export const StyledHeroIntro = styled.p`
 `
 
 export const StyledHeroTitle = styled.h1`
+    margin-block: 0 35px;
     font-size: 32px;
     font-weight: 900;
     line-height: 1;
@@ -21,9 +27,10 @@ export const StyledHeroTitle = styled.h1`
 `
 
 export const StyledHeroDescription = styled.p`
+    margin-block: 0 32px;
     font-size: 20px;
     font-weight: 400;
     line-height: 1.4;
     letter-spacing: 2px;
     color: ${({ theme }) => theme.colors.textSecondary};
-`
+    `
