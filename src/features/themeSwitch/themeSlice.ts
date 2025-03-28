6f3ea03b44c22412a0fca9fdc174ca548ae67c12
Jakub Musialowski <mds@mds.biz.pl>
2025-03-28 23:42:23 +0100

    Add theme slice

---
+++ src/features/themeSwitch/themeSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+
+interface ThemeState {
+    isDarkMode: boolean;
+}
+
+const initialState: ThemeState = {
+    isDarkMode: false,
+};
+
+const themeSlice = createSlice({
+    name: "theme",
+    initialState,
+    reducers: {
+        toogleTheme: (state: ThemeState) => {
+            state.isDarkMode = !state.isDarkMode;
+        }
+    }
+})
+
+export const themeStateSelector = (state: RootState) => state.theme;
+export const isDarkModeSelector = (state: RootState) => themeStateSelector(state).isDarkMode;
+
+export const { } = themeSlice.actions;
+export default themeSlice.reducer;
--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import repoReducer from "./features/repoList/repoSlice";
+import themeReducer from "./features/themeSwitch/themeSlice";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 export const store = configureStore({
     reducer: {
         repos: repoReducer,
+        theme: themeReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
