dc5a421e1de9b6b5fe56cde58e044b13cec175ec
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-01 22:16:50 +0200

    Add spinner img and styling to loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,10 +1,13 @@
 import { StyledSubtitle } from "../../common/styled/styled";
+import spinner from "../../common/images/icon-spinner.png";
+import { StyledLoading } from "./styled";
 
 const Loading: React.FC = () => {
     return (
-        <div>
+        <StyledLoading>
             <StyledSubtitle>Please wait, projects are being loaded...</StyledSubtitle>
-        </div>
+            <img src={spinner} alt="Loading spinner" width={160} height={160} />
+        </StyledLoading>
     )
 };
 
