3ab436ff21523fc48d6b53c2baf41382ba62ced7
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-01 23:20:20 +0200

    Add animation and responsivness to loader

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,12 +1,12 @@
 import { StyledSubtitle } from "../../common/styled/styled";
 import spinner from "../../common/images/icon-spinner.png";
-import { StyledLoading } from "./styled";
+import { StyledLoading, StyledSpinner } from "./styled";
 
 const Loading: React.FC = () => {
     return (
         <StyledLoading>
             <StyledSubtitle>Please wait, projects are being loaded...</StyledSubtitle>
-            <img src={spinner} alt="Loading spinner" width={160} height={160} />
+            <StyledSpinner src={spinner} alt="Loading spinner" width={160} height={160} />
         </StyledLoading>
     )
 };
--- src/components/Loading/styled.tsx
+++ src/components/Loading/styled.tsx
@@ -1,10 +1,39 @@
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 export const StyledLoading = styled.div`
     width: 100%;
+    padding-block: 88px 68px;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     gap: 48px;
+
+    @media (width < 1024px){
+        padding-block: clamp(3rem, 1.8636rem + 5.6818vw, 5.5rem)
+        clamp(1.25rem, 0.1136rem + 5.6818vw, 3.75rem);
+    }
+`
+
+const rotate = keyframes`
+    0%{
+        transform: rotate(0deg)
+    }
+
+    100%{
+        transform: rotate(360deg)
+    }
+`
+
+const styles = css`
+    animation: ${rotate} 2s linear infinite running;
+`
+
+export const StyledSpinner = styled.img`
+    ${styles}
+
+    @media (width < 1024px){
+        width: clamp(8.125rem, 7.2727rem + 4.2614vw, 10rem);
+        height: auto;
+    }
 `
