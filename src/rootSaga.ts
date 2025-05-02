c3f1db30593a09df7d9dc17afbfc3b9f58619f19
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-02 23:30:39 +0200

    Add theme saga to root saga

--- src/rootSaga.ts
+++ src/rootSaga.ts
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { repoSaga } from "./features/repoList/repoSaga";
+import { themeSaga } from "./features/themeSwitch/themeSaga";
 
 export default function* rootSaga() {
     yield all(
         [
             repoSaga(),
+            themeSaga(),
         ]
     )
 };
