5edd42f67792b10bd099050e7878b6b2726b59c8
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-03 23:38:26 +0200

    Add error component text

---
+++ src/components/Error/styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { StyledInfoBlock, StyledSubtitle } from "../../common/styled/styled";
+
+export const StyledError = styled(StyledInfoBlock)`
+    gap: 32px;
+`
+
+export const StyledErrorInfo = styled(StyledSubtitle)`
+    margin: 0;
+    text-align: center;
+`
