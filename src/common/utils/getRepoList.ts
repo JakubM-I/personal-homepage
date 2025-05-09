4cfe9ba4c88bb54eefbf0caae0b18543b03c3db1
Jakub Musiałowski <mds@mds.biz.pl>
2025-05-09 23:14:22 +0200

    Add responsive width to social icons

--- src/common/utils/getRepoList.ts
+++ src/common/utils/getRepoList.ts
@@ -1,6 +1,6 @@
 export const getRepoList = async () => {
     try{
-        const response = await fetch("https://api.github.com/users/JakubM-I/repos2");
+        const response = await fetch("https://api.github.com/users/JakubM-I/repos");
         if(!response.ok){
             throw new Error(response.statusText);
         }
